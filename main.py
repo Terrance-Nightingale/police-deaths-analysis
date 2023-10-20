c722715fdb55e212ce8181ed67f3e13a516191d5
Terrance-Nightingale <130939937+Terrance-Nightingale@users.noreply.github.com>
2023-10-20 11:48:37 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# TODO 1) Import the police deaths data
+# TODO 2) Determine the data structure
+# TODO 3) Based on data structure, determine best questions to ask
+# TODO 4) Based on questions, determine best graphic representation method (pie chart, bar graph, etc.)
